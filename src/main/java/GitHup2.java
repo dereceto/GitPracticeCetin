7aa8764b873f8bbaf6f13b44595aa316d4423de3
cetindere <58632371+cetindere@users.noreply.github.com>
2020-07-06 16:34:14 -0400

    Update GitHup2.java

--- src/main/java/GitHup2.java
+++ src/main/java/GitHup2.java
@@ -1,5 +1,6 @@
 public class GitHup2 {
     public static void main(String[] args) {
         System.out.println("From GitHup2 class");
+        System.out.println("From githup 3th time GitHup2 class");
     }
 }

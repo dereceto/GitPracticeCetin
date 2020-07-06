25a0f84cfe2372b7a715107a8b1bb8128e1d9f4f
cetindere <cetinderedc@gmail.com>
2020-07-06 16:18:19 -0400

    upload 2. time

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -14,5 +14,7 @@ public class GitPractice {
 
         System.out.println("Hello Word!!!From github");
         System.out.println("Hello Word!!!From github");
+
+        System.out.println("Hello Word!!!From intellij 2. time");
     }
 }
